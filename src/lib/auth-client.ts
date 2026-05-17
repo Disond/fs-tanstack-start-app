a3fc07208effe77e5e2c3f23c749b68611fd8617
Disond <milosevic.petar4@gmail.com>
2026-05-17 16:20:29 +0200

    feat(auth): update auth client baseURL to use environment variable

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-    baseURL:
-        'https://fs-tanstack-start-2piaxv0zo-disonds-projects-4ccf354b.vercel.app',
+    baseURL: import.meta.env.VITE_APP_URL,
 })
